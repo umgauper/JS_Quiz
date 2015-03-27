665e1e665928e89b9b378c6d4bdf8c3cd5cc9c70
Una Gauper <umgauper@gmail.com>
2015-03-27 17:25:04 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/quiz');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var quiz = require('./routes/quiz');
+var data = require('./routes/data');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// required for passport
+app.use(session({ secret: 'yaaaypassportjs'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//require('./app/routes.js')(app, passport);
+
+
+
+
+/*makes db accessible to the router*/
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/quiz', quiz);
+app.use('/data', data);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ public/javascripts/ang_app.js
@@ -0,0 +1,67 @@
+var app = angular.module('ang_app', [])
+    .controller('quizController', ['$scope', '$http', function($scope, $http) {
+
+        $http.get('/data').success(function(data) { //get the questionSets
+            $scope.questionSets = data.questionSets;
+        });
+
+        var answers = {};
+
+        $scope.i = 0;   //TODO: maybe? on score page show chosen answer and correct answers?
+
+
+
+        function updateAnswers() {
+            answers[$scope.i] = $('input:checked').val();
+        }
+
+        function loadStoredAnswer() {
+            $('input:checked').prop('checked', false);
+            if(answers[$scope.i]) { // If there is a value for the current answer,
+
+                var ans = answers[$scope.i];
+                var radio = "input[value='" + ans + "']";
+                $(radio).prop('checked', true); //check the corresponding radio button.
+            }
+        }
+
+        $scope.calculateScore = function() {
+            $scope.score = 0;
+            var userAnswers = [];
+            for(var answer in answers) {
+                userAnswers.push(answers[answer]);
+            }
+
+            for(var j=0; j<$scope.questionSets.length; j++) {
+                if( userAnswers[j] == $scope.questionSets[j].correctAnswer) {
+                    $scope.score++;
+                }
+            }
+            alert($scope.score);
+       };
+
+
+        $scope.next = function() {
+
+            if ( $('input:checked').val() ) {
+                updateAnswers();
+                if($scope.i < $scope.questionSets.length) {
+                    $scope.i++;
+                    loadStoredAnswer();
+                }
+
+            } else {
+                $('#error').show();
+            }
+        };
+
+        $scope.prev = function() {
+
+            if($scope.i > 0) {
+                $scope.i--;
+                loadStoredAnswer();
+            }
+        };
+
+    }]);
+//try putting data into a separate route, that sends JSON data when there's a get request...see last answer here: http://stackoverflow.com/questions/15009664/accessing-get-variable-in-angular-function
