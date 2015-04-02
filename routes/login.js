afedc10d45156ac88a17d5e08feafb85e217855a
Una Gauper <umgauper@gmail.com>
2015-04-02 22:22:20 +0700

    Added post handling for logins

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,5 +7,12 @@ router.get('/', function(req, res) {
   res.render('login', { message: req.flash('loginMessage')});
 
 });
+
+// process the login form
+router.post('/', passport.authenticate('local-login', {
+  successRedirect : '/profile', // redirect to the secure profile section
+  failureRedirect : '/login', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
 
 module.exports = router;
