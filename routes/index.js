60ad873dff959c7bae91462cb1aaf16788229bcd
Una Gauper <umgauper@gmail.com>
2015-04-14 21:55:15 +0800

    modified procfile

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    //var db = req.db;
-    //console.log(req.db);
-    ////var collection = db.get('usercollection');
-    ////collection.find({}, {}, function(e, docs) {
-    ////    res.render('index', { title: 'Express', userlist: docs});
-    ////
-    ////});
+    res.render('index');
 
 });
 
